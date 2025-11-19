a96cb94f430a8695c6a08398ddeb97c684f06023
Ajaykarre563 <karreajay37@gmail.com>
2025-11-20 01:07:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './Header';
+import Product from './Product';
+import CartList from './CartList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Product />} />
+          <Route path="/cart" element={<CartList />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/redux/slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+      localStorage.setItem("cart", JSON.stringify(state.items));
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload.id
+      );
+      localStorage.setItem("cart", JSON.stringify(state.items));
+    },
+
+    clearAllItems: (state) => {
+      state.items = [];
+      localStorage.removeItem("cart");
+    },
+  },
+});
+
+export const { addItem, removeItem, clearAllItems } = cartSlice.actions;
+export default cartSlice.reducer;
